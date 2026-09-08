25143d714b02489c594a1ca2ae8cfe338e7fa837
Luckmanyudha16 <luckmanyudha1603@gmail.com>
2026-09-08 19:36:45 +0700

    Update sitemap.xml

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = "https://pura-group-bennyto.onrender.com";
+    const baseUrl = "https:/pura-group-bennyto.onrender.com";
 
     return {
         rules: {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = "https://bennyto-puragroup.onrender.com";
+    const baseUrl = "https://pura-group-bennyto.onrender.com";
 
     return [
         {
